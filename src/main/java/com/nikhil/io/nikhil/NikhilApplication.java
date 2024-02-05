ca5f94dc613e68189591bc3baaa4ca512169284b
unknown <sharmanikhil524@gmail.com>
2024-02-05 12:31:36 +0530

    started on 05 feb

---
+++ src/main/java/com/nikhil/io/nikhil/NikhilApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.io.nikhil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikhilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikhilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikhil/io/nikhil/NikhilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.io.nikhil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikhilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
